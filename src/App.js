35aee9511ab331601d8d7863b090842c4e7e7a54
Alex C. B <43941153+acbihm@users.noreply.github.com>
2019-05-04 20:45:35 -0500

    adding render and extend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
-// import Game from "./components/Game";
-import Header from "./components/Header"
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper"
+// // import Game from "./components/Game";
+// import Header from "./components/Header"
+// import Footer from "./components/Footer";
+// import Wrapper from "./components/Wrapper"
+import Game from "./components/Game"
 
 function App() {
   return (
-    <div>
-    <Wrapper>
-      <Header />
-      <Footer />
-      </Wrapper>
-    </div>
+    <Game />
   );
 }
 
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,11 +1,35 @@
 
-
 import React, { Component } from "react";
 import "./style.css";
 import Scorebar from "../Scorebar";
 import Header from "../Header";
 import Wrapper from "../Wrapper";
-import Image from "../Image";
+// import Image from "../Image";
+// import Scorebar from "../Scorebar";
 import Footer from "../Footer";
+// import data from "../../data.json";
 
-export default Game
+
+
+class Game extends Component {
+    // state = {
+    //     data,
+    //     score: 0,
+    //     topScore: 0
+    // };
+
+
+    render() {
+        return (
+            <div>
+                <Scorebar />
+                <Header />
+                <Wrapper>
+
+                </Wrapper>
+                <Footer />
+            </div>
+        );
+    }
+}
+export default Game;
