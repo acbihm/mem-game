5b14bd112a394135a6e55c5b8bc16cf514fff3f5
Alex C. B <43941153+acbihm@users.noreply.github.com>
2019-05-04 21:13:12 -0500

    trying to make score update

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,4 +1,17 @@
 import React from "react";
 import "./style.css";
 
+function Image(props) {
+    return (
+        <div
+            // role="img"
+            // aria-label="click item"
+            onClick={() => props.handleClick(props.id)}
+            style={{ backgroundImage: `url("${props.image}")` }}
+            className={`img-block${props.shake ? " shake" : ""}`}
+        />
+    );
+}
+
 export default Image;
+
